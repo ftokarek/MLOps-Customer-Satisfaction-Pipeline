c1226d9805e75e6d63f48938d6678076c52bb2c1
Franciszek Tokarek <franciszektokarek@gmail.com>
2025-10-21 04:39:59 +0200

    Update & Run pipeline

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,11 +1,11 @@
-from zenml import pipelines
+from zenml import pipeline
 
 from steps.ingest_data import ingest_df
 from steps.clean_data import clean_df
 from steps.model_train import train_model
 from steps.evaluation import evaluate_model
 
-@pipelines()
+@pipeline()
 def training_pipeline(data_path: str):
     df = ingest_df(data_path)
     clean_df(df)
