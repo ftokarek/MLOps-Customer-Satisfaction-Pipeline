7f9e049a4bbf5503ddfeb394013d5d061b0c9d73
Franciszek Tokarek <franciszektokarek@gmail.com>
2025-10-21 11:27:38 +0200

    add: MLFlow and Bug Fallbacks

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,30 +1,63 @@
 import logging
 import pandas as pd
+import mlflow
 
 from zenml import step
 from src.evaluation import MSE, RMSE, R2
 from sklearn.base import RegressorMixin
 from typing import Tuple
 from typing_extensions import Annotated
+from zenml.client import Client
 
-@step
-def evaluate_model(model: RegressorMixin,
-    X_test: pd.DataFrame,
-    y_test: pd.DataFrame,
-) -> Tuple[Annotated[float,"r2_score"], Annotated[float,"rmse"],
-]:
-    try:
-        prediction = model.predict(X_test)
-        mse_class = MSE()
-        mse = mse_class.calculate_scores(y_test, prediction)
-        
-        r2_class = R2()
-        r2 = r2_class.calculate_scores(y_test, prediction)
-        
-        rmse_class = RMSE()
-        rmse = rmse_class.calculate_scores(y_test, prediction)
-        
-        return r2, rmse,
-    except Exception as e:
-        logging.error(f"Error evaluating model: {e}")
-        raise e
+experiment_tracker = Client().active_stack.experiment_tracker
+
+if experiment_tracker is not None:
+    @step(experiment_tracker=experiment_tracker.name)
+    def evaluate_model(model: RegressorMixin,
+        X_test: pd.DataFrame,
+        y_test: pd.DataFrame,
+    ) -> Tuple[Annotated[float,"r2_score"], Annotated[float,"rmse"],
+    ]:
+        try:
+            prediction = model.predict(X_test)
+            mse_class = MSE()
+            mse = mse_class.calculate_scores(y_test, prediction)
+            mlflow.log_metric("mse", mse)
+            
+            r2_class = R2()
+            r2 = r2_class.calculate_scores(y_test, prediction)
+            mlflow.log_metric("r2", r2)
+            
+            rmse_class = RMSE()
+            rmse = rmse_class.calculate_scores(y_test, prediction)
+            mlflow.log_metric("rmse", rmse)
+            
+            return r2, rmse,
+        except Exception as e:
+            logging.error(f"Error evaluating model: {e}")
+            raise e
+else:
+    @step
+    def evaluate_model(model: RegressorMixin,
+        X_test: pd.DataFrame,
+        y_test: pd.DataFrame,
+    ) -> Tuple[Annotated[float,"r2_score"], Annotated[float,"rmse"],
+    ]:
+        try:
+            prediction = model.predict(X_test)
+            mse_class = MSE()
+            mse = mse_class.calculate_scores(y_test, prediction)
+            logging.info(f"MSE: {mse}")
+            
+            r2_class = R2()
+            r2 = r2_class.calculate_scores(y_test, prediction)
+            logging.info(f"R2: {r2}")
+            
+            rmse_class = RMSE()
+            rmse = rmse_class.calculate_scores(y_test, prediction)
+            logging.info(f"RMSE: {rmse}")
+            
+            return r2, rmse,
+        except Exception as e:
+            logging.error(f"Error evaluating model: {e}")
+            raise e
